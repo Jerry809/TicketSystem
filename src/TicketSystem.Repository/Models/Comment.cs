42006406f09c2a60dc48f8b86943d86091057207
Jerry <jerry.shih@ibm.com>
2022-01-12 13:54:34 +0800

    add colunm add Table

---
+++ src/TicketSystem.Repository/Models/Comment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace TicketSystem.Repository.Models
+{
+    [Keyless]
+    [Table("Comment")]
+    public partial class Comment
+    {
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName = "ntext")]
+        public string Description { get; set; }
+        public int CreatorId { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime CreationTime { get; set; }
+    }
+}
---
+++ src/TicketSystem.Repository/Models/TicketHistory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace TicketSystem.Repository.Models
+{
+    [Keyless]
+    [Table("TicketHistory")]
+    public partial class TicketHistory
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(500)]
+        public string Summary { get; set; }
+        [Required]
+        [Column(TypeName = "ntext")]
+        public string Description { get; set; }
+        public int Type { get; set; }
+        public int? Severity { get; set; }
+        public int? Priority { get; set; }
+        public int? AsigneeUserId { get; set; }
+        public int Status { get; set; }
+        public int CreatorId { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime CreationTime { get; set; }
+    }
+}
